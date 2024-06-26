98c31446f1eb8092f8a773c95bb738a17d2df4d9
PLeX0 <49524128+PLeX0@users.noreply.github.com>
2024-06-26 13:41:11 +0200

    assetUpdate

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,39 +7,49 @@ public class Movement : MonoBehaviour
     public Rigidbody2D playerRb;
     public float speed;
     public AudioSource audioSource;
+    public Animator animator;
+    public Player player; 
 
-     
     // Start is called before the first frame update
     void Start()
     {
+
         playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        playerRb.velocity = (movement * speed* Time.deltaTime);
-
-        if(playerRb.velocity.x != 0)
+        if(!player.isDead)
         {
-            if(!audioSource.isPlaying)
+            Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            playerRb.velocity = (movement * speed * Time.deltaTime);
+
+            if (playerRb.velocity.x != 0)
             {
-                audioSource.Play();
+                animator.SetBool("isMoving", true);
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+            }
+            else if (playerRb.velocity.y != 0)
+            {
+                animator.SetBool("isMoving", true);
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+            }
+            else
+            {
+                audioSource.Stop();
+                animator.SetBool("isMoving", false);
             }
         }
-        else if (playerRb.velocity.y != 0)
-        {
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-        }
-        else
-        {
-            audioSource.Stop();
-        }
+       
+
+       
         
 
 
